5e6a19878543450c12a1e8ab27c107ef5260178c
Panagiotis Baklesis <baklesis@users.noreply.github.com>
2020-05-08 12:16:51 +0300

    added Panos' classes, changed Ioanna's inits adding self argument

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 class Account:
-    def __init__(username,password,email):
+    def __init__(self, username, password, email):
         self._username=username
         self._password=password
         self._email=email
@@ -21,7 +21,7 @@ class Account:
         
 
 class Organizer(Account):
-    def __init__(fullname, organization):
+    def __init__(self, fullname, organization):
         self._fullname=fullname
         self._organization=organization
     
@@ -38,7 +38,7 @@ class Admin(Account):
     pass
 
 class Message:
-    def __init__(recipient,author,text):
+    def __init__(self, recipient, author, text):
         self._recipient=recipient
         self._author=author
         self._text=text
@@ -59,10 +59,65 @@ class Message:
 
         
 class OrganizerList:
-    def __init__(organizer_list):
+    def __init__(self, organizer_list):
         self._organizer_list=organizer_list
 
     def get_organizer_list():
         return self._organizer_list
     def set_organizer_list(organizer_list):
         self._organizer_list=organizer_list
+
+
+class Session:
+    def __init__(self, user, building, floor, filters):
+        self.user = user
+        self.building = building
+        self.floor = floor
+        self.filters = filters
+    
+class Building:
+    def __init__(self, name, organization, room_list):
+        self.name = name
+        self.organization = organization
+        self.room_list = room_list
+
+class BuildingList:
+    def __init__(self):
+        self._building_list= building_list
+    
+    @property    
+    def building_list(self):
+        return self._building_list
+    
+    @building_list.setter
+    def building_list(self, ):
+        self._building_list= building_list
+
+    @building_list.deleter
+    def building_list(self):
+        del self._building_list
+
+class Room:
+    def __init__(self, name, building, floor, capacity, group):
+        self.name = name
+        self.building = building
+        self.floor = floor
+        self.capacity = capacity
+        self.group = group
+    
+
+
+class RoomList:
+    def __init__(self, room_list):
+        self.room_list = room_list
+    @property    
+    def room_list(self):
+        return self._room_list
+    
+    @room_list.setter
+    def room_list(self, ):
+        self._room_list=room_list
+
+    @room_list.deleter
+    def room_list(self):
+        del self._room_list
