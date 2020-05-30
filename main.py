68d4782dc4562d3ea4e4b5430e032abfb83a33b9
Ioanna Gogou <ioannagogou1998@gmail.com>
2020-05-30 19:56:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QDialog
 
-from gui_classes.MainWindow import Ui_MainWindow
+from ui_classes.MainWindow import MainWindow
 
 if __name__ == "__main__":
     MasterPlan = QApplication()
 
-    MainWindow = QMainWindow()
-
-    ui_MainWindow = Ui_MainWindow()
-    ui_MainWindow.setupUi(MainWindow)
-    ui_MainWindow.connectSignals()
-
-
-    MainWindow.show()
+    mainWindow = MainWindow()
+    mainWindow.show()
 
     sys.exit(MasterPlan.exec_())
