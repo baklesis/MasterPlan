3d1439bde780316ab5dd68f13f81f0869d9c0909
Panagiotis Baklesis <panagiotisbaklesis@gmail.com>
2020-06-06 18:28:09 +0300

    Starting implementation of Search functionality

--- ui_classes/SearchWindow.py
+++ ui_classes/SearchWindow.py
@@ -4,71 +4,76 @@ from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
     QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
     QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
-import schedule
+from global_vars import schedule
 
 
 class Ui_SearchWindow(object):
-    def setupUi(self, searchbtn):
-        if not searchbtn.objectName():
-            searchbtn.setObjectName(u"searchbtn")
-        searchbtn.resize(320, 320)
+    def setupUi(self, SearchWindow):
+        if not SearchWindow.objectName():
+            SearchWindow.setObjectName(u"SearchWindow")
+        SearchWindow.resize(320, 320)
         sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(searchbtn.sizePolicy().hasHeightForWidth())
-        searchbtn.setSizePolicy(sizePolicy)
-        searchbtn.setMinimumSize(QSize(320, 320))
-        searchbtn.setMaximumSize(QSize(320, 320))
-        self.lineEdit = QLineEdit(searchbtn)
-        self.lineEdit.setObjectName(u"Search")
-        self.lineEdit.setGeometry(QRect(30, 30, 171, 21))
-        self.lineEdit.setMaximumSize(QSize(16777215, 100))
-        self.searchButton = QPushButton(searchbtn)
+        sizePolicy.setHeightForWidth(SearchWindow.sizePolicy().hasHeightForWidth())
+        SearchWindow.setSizePolicy(sizePolicy)
+        SearchWindow.setMinimumSize(QSize(320, 320))
+        SearchWindow.setMaximumSize(QSize(320, 320))
+        self.searchBar = QLineEdit(SearchWindow)
+        self.searchBar.setObjectName(u"searchBar")
+        self.searchBar.setGeometry(QRect(30, 30, 171, 21))
+        self.searchBar.setMaximumSize(QSize(16777215, 100))
+        self.searchButton = QPushButton(SearchWindow)
         self.searchButton.setObjectName(u"searchButton")
         self.searchButton.setGeometry(QRect(200, 30, 31, 21))
         icon = QIcon()
-        icon.addFile(u"icons/search.png", QSize(), QIcon.Normal, QIcon.Off)
+        icon.addFile(u"../icons/search.png", QSize(), QIcon.Normal, QIcon.Off)
         self.searchButton.setIcon(icon)
-        self.horizontalLayoutWidget = QWidget(searchbtn)
+        self.horizontalLayoutWidget = QWidget(SearchWindow)
         self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
         self.horizontalLayoutWidget.setGeometry(QRect(30, 30, 201, 21))
         self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.listView = QListView(searchbtn)
-        self.listView.setObjectName(u"listView")
-        self.listView.setGeometry(QRect(30, 50, 201, 191))
-        self.verticalLayoutWidget = QWidget(searchbtn)
+        self.searchResults = QListView(SearchWindow)
+        self.searchResults.setObjectName(u"searchResults")
+        self.searchResults.setGeometry(QRect(30, 50, 201, 191))
+        self.verticalLayoutWidget = QWidget(SearchWindow)
         self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
         self.verticalLayoutWidget.setGeometry(QRect(30, 30, 201, 211))
         self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
 
-        self.retranslateUi(searchbtn)
+        self.retranslateUi(SearchWindow)
 
-        QMetaObject.connectSlotsByName(searchbtn)
+        QMetaObject.connectSlotsByName(SearchWindow)
+    # setupUi
 
-    def retranslateUi(self, searchbtn):
-        SearchWindow.setWindowTitle(QCoreApplication.translate("SearchWindow", u"Download", None))
-        searchbtn.setWindowTitle(QCoreApplication.translate("Search Events", u"Form", None))
+    def retranslateUi(self, SearchWindow):
+        SearchWindow.setWindowTitle(QCoreApplication.translate("SearchWindow", u"Form", None))
         self.searchButton.setText("")
+    # retranslateUi
 
 class SearchWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_SearchWindow()
         self.ui.setupUi(self)
-        self.completer = QCompleter(schedule.event_list)
+        self.event_name_list = [x.name for x in schedule.event_list]
+        self.completer = QCompleter(schedule.event_name_list)
         self.completer.setCaseSensitivity(Qt.CaseInsensitive)
-        self.searchbar.setCompleter(self.completer)
+        self.ui.searchBar.setCompleter(self.completer)
+
+    def connectSignals(self):
+        self.ui.searchButton.clicked.connect(update_display)
 
     def showWindow(self):
         self.show()
 
     def update_display(self, text):
 
-        for event in schedule.event_list:
+        for event in schedule.event_name_list:
             if text.lower() in event.name.lower():
                 event.show()
             else:
